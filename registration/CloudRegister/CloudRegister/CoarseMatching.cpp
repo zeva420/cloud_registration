5938e6cb058e3fe318933a3591954f5f19bc8b36
egoal <939425550@qq.com>
2020-11-25 17:07:44 +0800

    check z slice on cad, ambiguilty removed.

--- registration/CloudRegister/CloudRegister/CoarseMatching.cpp
+++ registration/CloudRegister/CloudRegister/CoarseMatching.cpp
@@ -1,8 +1,6 @@
 #include "CoarseMatching.h"
 #include "glog/logging.h"
 
-#include "GeometryUtils.h"
-
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/ransac.h>
@@ -38,20 +36,23 @@ std::string CoarseMatching::MatchResult::toString() const {
 CoarseMatching::CoarseMatching() {}
 
 CoarseMatching::MatchResult CoarseMatching::run(const std::vector<PointCloud::Ptr>& allPieces, const CADModel& cadModel) {
-	const auto& holes = cadModel.getTypedModelItems(ITEM_HOLE_E);
-	for (const auto& hole : holes) LOG(INFO) << hole.toString();
-
 	constexpr float HOR_CHECK_EPS = 0.01f;
 
 	LOG(INFO) << "coarse match " << allPieces.size() << " walls.";
 
+	//0. downsample
+	const auto sparsedPieces = ll::mapf([](PointCloud::Ptr cloud) { return geo::downsampleUniformly(cloud, 0.01f); }, allPieces);
+	// debug write.
+	for (auto pr : ll::enumerate(sparsedPieces))
+		pcl::io::savePCDFile(ll::unsafe_format("coarse-wall-%d.pcd", pr.index), *(*pr.iter), true);
+
 	//1. basically, we use walls to do coarse matching.
 	std::vector<std::pair<PointCloud::Ptr, Segment>> wallPieces;
-	wallPieces.reserve(allPieces.size()); // may -2
+	wallPieces.reserve(sparsedPieces.size()); // may -2
 	PointCloud::Ptr floor, roof; // still we need floor and roof to get the proper Y
 	float zFloor, zRoof;
 
-	for (auto pr : ll::enumerate(allPieces)) {
+	for (auto pr : ll::enumerate(sparsedPieces)) {
 		auto cloud = *pr.iter;
 
 		PointCloud::Ptr plane;
@@ -80,62 +81,6 @@ CoarseMatching::MatchResult CoarseMatching::run(const std::vector<PointCloud::Pt
 	LOG(INFO) << ll::unsafe_format("%d walls found, floor %.3f -> roof %.3f, total height: %.3f.",
 		wallPieces.size(), zFloor, zRoof, (zRoof - zFloor));
 	LOG_IF(INFO, wallPieces.size() % 2 != 0) << "note that wall size is odd, we may missed some.";
-
-	if (1) {
-		constexpr float MARGIN = 0.02f;
-		constexpr float Z = 1.f;
-
-		auto bpm = intersectCADModelOnZ(cadModel, Z);
-
-		pcl::visualization::PCLVisualizer viewer;
-
-		const float wallz = zFloor + Z;
-
-		for (const auto& pr : ll::enumerate(wallPieces)) {
-			const auto& wallpr = *pr.iter;
-			auto line = geo::passThrough(wallpr.first, "z", wallz - MARGIN, wallz + MARGIN);
-			Eigen::VectorXf params(6, 1);
-			params.block<3, 1>(0, 0) = wallpr.second.s_;
-			params.block<3, 1>(3, 0) = wallpr.second.dir();
-			LOG(INFO) << "passthrough: " << line->size() << " / " << wallpr.first->size() << ", line: " << params.transpose();
-
-			const auto subsegs = splitSegments(line, params, 0.1f, 0.1f);
-
-			// show
-			double r{ geo::random() }, g{ geo::random() }, b{ geo::random() };
-
-			pcl::visualization::PointCloudColorHandlerCustom<Point> color(line, r * 255., g * 255., b * 255.);
-			viewer.addPointCloud(line, color, "piece" + std::to_string(pr.index));
-
-			for (auto segpr : ll::enumerate(subsegs)) {
-				const auto& seg = *segpr.iter;
-
-				viewer.addSphere(geo::P_(seg.s_), 0.1f, 1., 1., 0.,
-					"seg-s-" + std::to_string(pr.index) + "-" + std::to_string(segpr.index));
-				viewer.addSphere(geo::P_(seg.e_), 0.1f, 1., 1., 0.,
-					"seg-e-" + std::to_string(pr.index) + "-" + std::to_string(segpr.index));
-				// viewer.addText3D(std::to_string(pr.index), Point(p.x, p.y, 0.2f), 0.3f);
-			}
-		}
-
-		auto cad = cadModel.genTestCloud();
-		pcl::visualization::PointCloudColorHandlerCustom<Point> color(cad, 255., 0., 0.);
-		viewer.addPointCloud(cad, color, "cad");
-
-		for (auto pr : ll::enumerate(bpm)) {
-			const Eigen::Vector3d& v = *pr.iter;
-			Point p(v(0), v(1), v(2));
-
-			viewer.addSphere(p, 0.1f, "outline" + std::to_string(pr.index));
-			viewer.addText3D(std::to_string(pr.index), Point(p.x, p.y, 0.2f), 0.3f);
-		}
-
-		while (!viewer.wasStopped()) {
-			viewer.spinOnce(33);
-		}
-
-		return CoarseMatching::MatchResult();
-	}
 
 	//2. setup outline
 	Eigen::vector<Eigen::Vector2f> cens(wallPieces.size());
@@ -189,7 +134,28 @@ CoarseMatching::MatchResult CoarseMatching::run(const std::vector<PointCloud::Pt
 	std::transform(blueprint3d.points_.begin(), blueprint3d.points_.end(), blueprint.begin(),
 		[](const Eigen::Vector3d& v) { return Eigen::Vector2f(v(0, 0), v(1, 0)); });
 
-	Eigen::Matrix4f T = computeOutlineTransformation(blueprint, outline, BLUEPRINT_MATCH_DIS_THRESH);
+	Eigen::Matrix4f T = Eigen::Matrix4f::Identity();
+	{
+		const auto& Ts = computeOutlineTransformCandidates(blueprint, outline, BLUEPRINT_MATCH_DIS_THRESH);
+		if (Ts.size() == 1) T = trans2d::asTransform3d(Ts.front());
+		else {
+			std::vector<PointCloud::Ptr> walls;
+			std::vector<Eigen::VectorXf> wallparams;
+			{
+				walls.reserve(wallPieces.size());
+				wallparams.reserve(wallPieces.size());
+
+				for (const auto& pr : wallPieces) {
+					walls.push_back(pr.first);
+					Eigen::VectorXf params(6, 1);
+					params.block<3, 1>(0, 0) = pr.second.s_;
+					params.block<3, 1>(3, 0) = pr.second.dir();
+					wallparams.push_back(params);
+				}
+			}
+			T = trans2d::asTransform3d(chooseTransformByHoles(cadModel, walls, wallparams, Ts, zFloor));
+		}
+	}
 	T(2, 3) = -zFloor; // shift to zero
 	LOG(INFO) << "T: \n" << T << "\n";
 
@@ -226,10 +192,6 @@ CoarseMatching::MatchResult CoarseMatching::run(const std::vector<PointCloud::Pt
 	}
 
 	LOG(INFO) << result.toString();
-
-	// debug write.
-	for (auto pr : ll::enumerate(allPieces))
-		pcl::io::savePCDFile(ll::unsafe_format("coarse-wall-%d.pcd", pr.index), *(*pr.iter), true);
 
 	// simple visualization
 	pcl::visualization::PCLVisualizer viewer;
@@ -321,8 +283,7 @@ CoarseMatching::Segment CoarseMatching::detectMainSegementXoY(PointCloud::Ptr cl
 		LOG(WARNING) << "initial plane n.z: " << plane_param[2] << ", may not a proper call.";
 	}
 
-	PointCloud::Ptr sparsed = geo::downsampleUniformly(cloud, 0.01f);
-	PointCloud::Ptr cloud2d = geo::mapPoints(sparsed, [](const Point& p) { return Point(p.x, p.y, 0.f); });
+	PointCloud::Ptr cloud2d = geo::mapPoints(cloud, [](const Point& p) { return Point(p.x, p.y, 0.f); });
 
 	LOG(INFO) << "filter|map to 2d: " << cloud->size() << " -> " << cloud2d->size();
 
@@ -359,7 +320,7 @@ CoarseMatching::Segment CoarseMatching::detectMainSegementXoY(PointCloud::Ptr cl
 	return segment;
 }
 
-Eigen::Matrix4f CoarseMatching::computeOutlineTransformation(const Eigen::vector<Eigen::Vector2f>& blueprint,
+Eigen::vector<trans2d::Matrix2x3f> CoarseMatching::computeOutlineTransformCandidates(const Eigen::vector<Eigen::Vector2f>& blueprint,
 	const Eigen::vector<Eigen::Vector2f>& outline, float disthresh) {
 	// try to align by each wall
 	struct can {
@@ -437,7 +398,8 @@ Eigen::Matrix4f CoarseMatching::computeOutlineTransformation(const Eigen::vector
 
 	if (candidates.front().maxmindis > disthresh) {
 		LOG(INFO) << "no candidates found, match failed!\n";
-		return Eigen::Matrix4f::Identity();
+		auto I = trans2d::buildTransform(0.f, Eigen::Vector2f::Zero());
+		return Eigen::vector<trans2d::Matrix2x3f>(1, I);
 	}
 
 	// remove duplicate match pattern by transformation
@@ -490,11 +452,124 @@ Eigen::Matrix4f CoarseMatching::computeOutlineTransformation(const Eigen::vector
 
 	LOG(INFO) << candidates.size() << " UNIQUE candidates found:\n";
 	for (auto pr : ll::enumerate(candidates))
-		LOG(INFO) << pr.index << "| " << pr.iter->to_string() << "\n";
+		LOG(INFO) << pr.index << "| " << pr.iter->to_string();
 
-	//todo: handle the case where #candidates /= 1
+	Eigen::vector<trans2d::Matrix2x3f> Ts;
+	Ts.reserve(candidates.size());
+	for (const auto& c : candidates) Ts.emplace_back(c.T);
 
-	return trans2d::asTransform3d(candidates.front().T);
+	return Ts;
+}
+
+trans2d::Matrix2x3f CoarseMatching::chooseTransformByHoles(const CADModel& cadModel,
+	const std::vector<PointCloud::Ptr>& walls, const std::vector<Eigen::VectorXf>& wallparams,
+	const Eigen::vector<trans2d::Matrix2x3f>& Ts, const float floorZ) const {
+
+	constexpr float HALF_SLICE_THICKNESS = 0.02f;
+
+	// todo: we may check order
+	// T* src = dst, return #{matched srcs}
+	auto get_matched_num = [](const Eigen::vector<Eigen::Vector2f>& src,
+		const Eigen::vector<Eigen::Vector2f>& dst, const trans2d::Matrix2x3f& T) -> std::size_t {
+		Eigen::vector<Eigen::Vector2f> transed_src(src.size());
+		std::transform(src.begin(), src.end(), transed_src.begin(), [&T](const Eigen::Vector2f& v) { return trans2d::transform(v, T); });
+
+		return std::count_if(transed_src.begin(), transed_src.end(), [&dst](Eigen::Vector2f& sp) {
+			return std::any_of(dst.begin(), dst.end(), [&sp](const Eigen::Vector2f& dp) { return (sp - dp).norm() < 0.1f; });
+		});
+	};
+
+	auto get_consistent_transformations_for = [&](const Eigen::vector<trans2d::Matrix2x3f>& checkTs, float z)-> Eigen::vector<trans2d::Matrix2x3f> {
+		Eigen::vector<Eigen::Vector3d> cadHoleEnds = intersectCADModelOnZ(cadModel, z);
+		if (cadHoleEnds.empty()) {
+			LOG(WARNING) << "bad function call: no intersection points found at: " << z;
+			return Ts;
+		}
+
+		// slice and detect ends
+		Eigen::vector<Eigen::Vector3f> wallEnds;
+		for (auto pr : ll::enumerate(walls)) {
+			auto wall = *pr.iter;
+			auto params = wallparams[pr.index];
+
+			auto line = geo::passThrough(wall, "z", floorZ + z - HALF_SLICE_THICKNESS, floorZ + z + HALF_SLICE_THICKNESS);
+
+			auto subsegs = splitSegments(line, params, 0.1f, 0.1f);
+
+			for (const auto& seg : subsegs) {
+				wallEnds.emplace_back(seg.s_);
+				wallEnds.emplace_back(seg.e_);
+			}
+		}
+
+		Eigen::vector<Eigen::Vector2f> hole2d, wall2d;
+		{
+			hole2d.reserve(cadHoleEnds.size());
+			wall2d.reserve(wallEnds.size());
+			for (const auto& v : cadHoleEnds) hole2d.emplace_back(v(0), v(1));
+			for (const auto& v : wallEnds) wall2d.emplace_back(v(0), v(1));
+		}
+
+		// now check if matched
+		Eigen::vector<trans2d::Matrix2x3f> re;
+		for (const auto& T : checkTs) {
+			// succeed if only each cad hole have candidate
+			std::size_t cnt = get_matched_num(hole2d, wall2d, trans2d::inverse(T));
+
+			if (cnt == cadHoleEnds.size()) re.emplace_back(T);
+		}
+
+		return re;
+	};
+
+	// choose height
+	if (!cadModel.containModels(ITEM_HOLE_E)) {
+		LOG(WARNING) << "no holes in cad model! choose the first one (with least max min distance).";
+		return Ts.front();
+	}
+
+	const auto& holes = cadModel.getTypedModelItems(ITEM_HOLE_E);
+	auto hranges = ll::mapf([](const ModelItem& mi) { return mi.highRange_; }, holes);
+
+	const double low = ll::min_by(ll::get_key<double, double>, hranges).second;
+	const double high = ll::max_by(ll::get_value<double, double>, hranges).second;
+	LOG(INFO) << "hole high range: " << low << " -> " << high << " (" << (high - low) << ")";
+
+	// this can be very inefficient for now.
+	// todo: rethink & optimize this process, use grid for now.
+	constexpr double GRID_SIZE = HALF_SLICE_THICKNESS * 2.5;
+	const int N = static_cast<int>((high - low) / GRID_SIZE) + 1;
+	std::vector<int> cell_counts(N, 0);
+	for (const auto& hr : hranges) {
+		int ilow = (hr.first - low) / GRID_SIZE;
+		int ihigh = (hr.second - low) / GRID_SIZE;
+
+		for (int i = ilow; i <= ihigh; ++i) cell_counts[i]++;
+	}
+	LOG(INFO) << "cell counts: " << ll::string_join(cell_counts, ", ");
+
+	// only avoid jumps for now...
+	Eigen::vector<trans2d::Matrix2x3f> leftTs = Ts;
+	for (std::size_t i = 1; i < cell_counts.size() - 1; ++i) {
+		if (cell_counts[i - 1] == cell_counts[i] && cell_counts[i] == cell_counts[i + 1] && cell_counts[i] != 0) {
+			float mid = low + i * GRID_SIZE + GRID_SIZE / 2.f;
+
+			auto lTs = get_consistent_transformations_for(leftTs, mid);
+			if (lTs.empty()) {
+				LOG(WARNING) << "no consistent transformation at: " << mid;
+			} else if (lTs.size() == 1) {
+				LOG(INFO) << "unique consistence check succeed at: " << mid;
+				return lTs.front();
+			} else {
+				LOG(INFO) << lTs.size() << " consistent transformation found at: " << mid;
+				leftTs.swap(lTs);
+			}
+		}
+	}
+
+	// we failed to find any
+	LOG(WARNING) << "failed to remove ambiguity, left candidates: " << leftTs.size() << "/" << Ts.size() << ". choose the first one (with least max min distance).";
+	return leftTs.front();
 }
 
 std::vector<CoarseMatching::Segment> CoarseMatching::detectSegmentsXOY(PointCloud::Ptr cloud,
@@ -563,9 +638,9 @@ std::vector<CoarseMatching::Segment> CoarseMatching::splitSegments(PointCloud::P
 	}
 
 	auto cnt = std::count_if(sets.begin(), sets.end(), [](const std::unordered_set<std::size_t>& c) { return c.size() > 1; });
-	LOG(INFO) << cnt << " cluster(s) found.";
 
 	std::vector<Segment> segments;
+	segments.reserve(cnt);
 	for (const auto& ids : sets) {
 		if (ids.size() < 2) continue;
 
@@ -581,6 +656,8 @@ std::vector<CoarseMatching::Segment> CoarseMatching::splitSegments(PointCloud::P
 
 		segments.push_back(s);
 	}
+
+	LOG(INFO) << segments.size() << " / " << cnt << " segment(s) found.";
 
 	return segments;
 }
@@ -598,8 +675,7 @@ Eigen::vector<Eigen::Vector3d> CoarseMatching::intersectCADModelOnZ(const CADMod
 
 	// we now try intersect each hole
 	// note that the hole can be non-convex, so we only need intersection points
-	auto points = bottom_outline;
-	for (Eigen::Vector3d& p : points) p(2) = z;
+	Eigen::vector<Eigen::Vector3d> points;
 
 	const auto& holes = cadModel.getTypedModelItems(ITEM_HOLE_E); // we already checked this in above
 
@@ -620,17 +696,28 @@ Eigen::vector<Eigen::Vector3d> CoarseMatching::intersectCADModelOnZ(const CADMod
 		points.insert(points.end(), ips.begin(), ips.end());
 	}
 
-	LOG(INFO) << points.size() << "/" << bottom_outline.size() << " intersection points found on z: " << z;
+	LOG(INFO) << points.size() << " intersection points found on z: " << z;
 
-	if (points.size() != bottom_outline.size()) {
-		Eigen::vector<Eigen::Vector2f> points2d(points.size());
-		std::transform(points.begin(), points.end(), points2d.begin(), [](const Eigen::Vector3d& p) { return Eigen::Vector2f(p(0, 0), p(1, 0));  });
+	if (!points.empty()) {
+		// remove duplicated points
+		// if points are too close, then this may NOT be an detectable point
+		constexpr double CHECK_THRESH = 0.01;
+		std::vector<Eigen::vector<Eigen::Vector3d>> sets; // a simple cluster
+		for (const auto& p : points) {
+			auto iter = std::find_if(sets.begin(), sets.end(), [&p, CHECK_THRESH](const Eigen::vector<Eigen::Vector3d>& set) {
+				return std::any_of(set.begin(), set.end(), [&p, CHECK_THRESH](const Eigen::Vector3d& q) { return (p - q).norm() < CHECK_THRESH; });
+			});
+			if (iter == sets.end()) {
+				sets.emplace_back(1, p);
+			} else {
+				iter->emplace_back(p);
+			}
+		}
 
-		auto ids = geo::sort_points_counter_clockwise(points2d);
-		Eigen::vector<Eigen::Vector3d> sorted(points.size());
-		for (auto pr : ll::enumerate(ids))
-			sorted[pr.index] = points[*pr.iter];
-		points.swap(sorted);
+		points.clear();
+		for (const auto& s : sets) if (s.size() == 1) points.emplace_back(s.front());
+
+		LOG(INFO) << points.size() << " valid points found.";
 	}
 
 	return points;
